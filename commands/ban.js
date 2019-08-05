52af7eb4280376c5e000ea253a190f14df624ba7
Joseph <josephgoh7@gmail.com>
2019-08-05 19:36:28 +0800

    ban fix #3

--- commands/ban.js
+++ commands/ban.js
@@ -18,7 +18,13 @@ module.exports = class Ban extends Command {
 			reason = params.join(" "),
 			user = await this.helpers.fetchUser(mention, msg.guild);
 		if(!user)reply.throw("Invalid discord user.");
-		const [p1, banInfo] = await Promise.all([ msg.guild.fetchBan(user), msg.guild.ban(user, { reason }) ]);
+		const [p1, banInfo] = await Promise.all([ 
+			msg.guild.fetchBan(user).catch(err => {
+				if(e.message === 'Unknown Ban')return null;
+				throw err;
+			}),
+			msg.guild.ban(user, { reason }) 
+		]);
 		reply.channel = this.self.production ? msg.guild.channels.get("416248602227376128") : msg.guild.channels.find(ch => ch.name === "testing");
 		if(banInfo)return reply.append(`User ${ user.tag } (ID: ${ user.id }) is ${ banInfo.reason ? ` due to **${ banInfo.reason }**` : "" }.`);
 		reply.append(`Successfully banned user ${ user.tag } (ID: ${ user.id }) ${ reason ? ` due to **${ reason }**` : "" }.`);
