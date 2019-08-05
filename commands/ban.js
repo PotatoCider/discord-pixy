ecfb64e03fe002b3dc04d2ebe616e34e617543d8
Joseph <josephgoh7@gmail.com>
2019-08-05 20:06:46 +0800

    ban tweak #9

--- commands/ban.js
+++ commands/ban.js
@@ -18,14 +18,14 @@ module.exports = class Ban extends Command {
 			reason = params.join(" "),
 			user = await this.helpers.fetchUser(mention, msg.guild)
 		if(!user)reply.throw("Invalid discord user.");
-		const [p1, banInfo] = await Promise.all([ 
-			msg.guild.ban(user, { reason }),
-			msg.guild.fetchBan(user).catch(err => {
-				if(err.message === 'Unknown Ban')return null;
-				throw err;
-			})
-		]);
+		
+		const banInfo = await msg.guild.fetchBan(user).catch(err => {
+			if(err.message === 'Unknown Ban')return null;
+			throw err;
+		})
 		if(banInfo)return reply.append(`User ${ user.tag } (ID: ${ user.id }) is already banned${ banInfo.reason ? ` due to **${ banInfo.reason }**` : " with no reason given" }.`);
+
+		await msg.guild.ban(user, { reason });
 		reply.channel = this.self.production ? msg.guild.channels.get("416248602227376128") : msg.guild.channels.find(ch => ch.name === "testing");
 		reply.append(`Successfully banned user ${ user.tag } (ID: ${ user.id }) ${ reason ? ` due to **${ reason }**` : "" }.`);
 		msg.react('✅');
