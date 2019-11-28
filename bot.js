52d02f3733b044d22345ebb9c9665eea5f664a5b
Joseph Goh <josephgoh7@gmail.com>
2019-11-28 21:00:35 +0800

    Modified bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,3 @@ const Self = require("./util/Self"),
 	self = new Self();
 
 self.start();
-
-/*
-	On 1 Jan 2019: Farewell Xenocide.
-	 - Send a message in #main_lounge about my reason of leaving Xenocide.
-	 - End of support 1 Jan 2020.
-	 - WhatsApp is always avaliable for chat/issues of Kiz
-	 - Leaving discord as it invites you to play games.
-	 - What I will be doing: Coding on self projects/Preparing for my future.
-	 - Should not be taken as bad news as I am improving on my life.
-	 - Say goodbye to Kuro, Kai, Icey, and Ero
-	 - Create an online repo of Kizuna Ai -> incase anyone interested lmao
-*/
